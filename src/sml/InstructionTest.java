c5d032ddd15ab103ae47f2791d7d6642cb7eec4a
BBK-SDP-2015-dlord03 <dlord03@dcs.bbk.ac.uk>
2015-01-26 19:44:48 +0000

    Tidy up.

--- src/sml/InstructionTest.java
+++ src/sml/InstructionTest.java
@@ -6,12 +6,12 @@ public class InstructionTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void failToCreateInstructionWithWrongCode() {
-		Instruction instruction = new LinInstruction("f2", "mul");
+		new LinInstruction("f2", "mul");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void failToCreateInstructionWithWrongArguments() {
-		Instruction instruction = new LinInstruction("f2", new String[] {"23"});
+		new LinInstruction("f2", new String[] {"23"});
 	}
 
 }
